3aff5fd701501f51f8ad499725fd1b625eb7e45c
batt0s <kerem.ullen@outlook.com>
2022-03-27 00:48:48 +0300

    Error handling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
 from utils import getQuote
 
 app = FastAPI()
@@ -6,19 +7,29 @@ app = FastAPI()
 @app.get("/")
 def random():
     quote, saidBy, link = getQuote()
+    if quote == '':
+        data = {
+            "error": "An error occurred."
+        }
+        return JSONResponse(content=data, status_code=status.HTTP_400_BAD_REQUEST)
     data = {
         "quote": quote,
         "saidBy": saidBy,
         "link": link
     }
-    return data
+    return JSONResponse(content=data, status_code=status.HTTP_200_OK)
 
 @app.get("/{url}")
 def custom(url):
     quote, saidBy, link = getQuote(url)
+    if quote == '':
+        data = {
+            "error": "An error occurred."
+        }
+        return JSONResponse(content=data, status_code=status.HTTP_400_BAD_REQUEST)
     data = {
         "quote": quote,
         "saidBy": saidBy,
         "link": link
     }
-    return data
+    return JSONResponse(content=data, status_code=status.HTTP_200_OK)
--- utils.py
+++ utils.py
@@ -5,12 +5,15 @@ headers = {
     'User-Agent': 'ea-quote',
 }
 def getQuote(url = "rastgele"):
-    req = requests.get(baseUrl+url, headers=headers)
-    html = req.text
-    soup = BeautifulSoup(html, 'html.parser')
-    a = soup.find("a", class_="quote")
-    quote = a.contents[0].strip('\n')
-    link = a['href']
-    saidByDiv = soup.find("div", class_="title")
-    saidBy = saidByDiv.contents[0]
-    return quote, saidBy, link
+    try:
+        req = requests.get(baseUrl+url, headers=headers)
+        html = req.text
+        soup = BeautifulSoup(html, 'html.parser')
+        a = soup.find("a", class_="quote")
+        quote = a.contents[0].strip('\n')
+        link = a['href']
+        saidByDiv = soup.find("div", class_="title")
+        saidBy = saidByDiv.contents[0]
+        return quote, saidBy, link
+    except:
+        return '','',''
